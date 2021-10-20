2a7ee6c43cfe83d091ed9798ea3d80c50aa65e65
andrei <git1@email.com>
2021-10-20 22:18:37 +0200

    Update Object.create().js

--- Object.create().js
+++ Object.create().js
@@ -7,7 +7,9 @@ let obj = {
     name: 'The Dude'
 };
 
-let obj2 = Object.create(obj, {});
+let obj2 = Object.create(obj, {
+    age: {value: 12, enumerable: true}
+});
 // console.log(obj2);
 // console.log(obj2.name);
 
@@ -25,6 +27,29 @@ let obj3 = Object.create(obj2, {
         value: true
     }
 });
-console.log(obj3);
-console.log(obj3.name);
+// console.log(obj3);
+// console.log(obj3.name);
+// console.log(obj3.__proto__);
+// console.log(Object.getOwnPropertyDescriptors(obj3.__proto__));
 
+// console.log(Object.__proto__);
+// console.log(Object.getOwnPropertyDescriptors(Object.__proto__));
+
+// console.log(Array.__proto__)
+// console.log(Object.getOwnPropertyDescriptors(Array.__proto__));
+console.log(Array.prototype)
+// console.log(Object.getOwnPropertyDescriptors(Array.prototype))
+console.log(Array)
+console.log([].__proto__)
+// console.log([] instanceof Array)
+// console.log(Object.getOwnPropertyDescriptors([].__proto__))
+
+// let ha = Object.name = 'me';
+// console.log(Object.getOwnPropertyDescriptors(ha.__proto__))
+// console.log(ha.valueOf)
+
+// console.log(Object.getPrototypeOf(obj3));
+// console.log(Object.getOwnPropertyDescriptors(Object.getPrototypeOf(obj3)));
+
+// console.log(Object.getPrototypeOf(Object.getPrototypeOf(obj3)));
+// console.log(Object.getOwnPropertyDescriptors(Object.getPrototypeOf(Object.getPrototypeOf(obj3))));
