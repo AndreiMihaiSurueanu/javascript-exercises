e008c387a53e6c9e6c39ff1f05705a629576e490
andrei <git1@email.com>
2022-01-08 09:34:10 +0100

    Git Script Update

--- git filter-branch sript.sh
+++ git filter-branch sript.sh
@@ -15,3 +15,8 @@ HEAD
 
 # git rebase -i HEAD~10 -x "git commit --amend --author 'andrei <git1@email.com>' --no-edit"
 # git rebase -i --root -x "git commit --amend --author 'andrei <git1@email.com>' --no-edit"
+# git rebase -i --root -x "git commit --amend --date='Wed Feb 16 14:00 2011 +0100' --no-edit"
+# git rebase -i aed76f1 master -x "GIT_COMMITTER_DATE='Mon Oct 18 14:00 2021 +0100' git commit --amend --date --no-edit"
+# git rebase -i aed76f1 master -x "GIT_COMMITTER_DATE='2020-09-22' git commit --amend --date='2020-09-22' --no-edit"
+# git rebase -i --root -x "GIT_COMMITTER_DATE='2021-02-09 12:00:00' git commit --amend --date='2021-02-09 12:00:00' --no-edit"
+# git rebase -i --root -x "GIT_COMMITTER_DATE='2021-03-17 10:09:41' git commit --amend --date='2021-03-17 10:09:41' --no-edit"
