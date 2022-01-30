fd91a41017b57e20777a0c781aa5a1a6e612af45
andrei <git1@email.com>
2022-01-30 11:00:34 +0100

    Generate Json File

--- paths/paths.js
+++ paths/paths.js
@@ -1,3 +1,18 @@
-var fs = require('fs');
-var files = fs.readdirSync('../');
+const fs = require("fs");
+const files = fs.readdirSync("../");
+const paths = {
+	paths: files,
+};
+
+// https://stackoverflow.com/questions/36856232/write-add-data-in-json-file-using-node-js
+fs.writeFile(
+	"paths.json",
+	JSON.stringify(paths),
+	"utf8",
+	function readFileCallback(err, data) {
+		if (err) {
+			console.log(err);
+		}
+	}
+);
 console.log(files);
