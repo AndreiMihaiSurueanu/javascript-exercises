2b84a4cf51e32d3f7729a22736711f2ae4aae609
andrei <git1@email.com>
2022-12-20 13:20:53 +0100

    Update findRemainigBalls.js
    * Intermediate Step 1

--- Algorithms/findRemainigBalls.js
+++ Algorithms/findRemainigBalls.js
@@ -9,11 +9,18 @@ let inputString = "";
 let currentLine = 0;
 
 process.stdin.on("data", function (inputStdin) {
+	// console.log('Reading: ' + inputStdin);
+	console.log('This is inputString before reading value:\n' + inputString)
+	console.log('inputString[0] before reading: ' + inputString[0])
 	inputString += inputStdin;
+	console.log('inputString[0] after: ' + inputString[0])
+	console.log('I added inputStdin to inputString:\n' + inputString);
 });
 
 process.stdin.on("end", function () {
+	console.log('End of reading: ');
 	inputString = inputString.split("\n");
+	console.log('Input string is: ' + inputString);
 
 	main();
 });
@@ -22,32 +29,71 @@ function readLine() {
 	return inputString[currentLine++];
 }
 
+/* if the array is empty then it means that there are no lines to read, return empty array
+if there is only one ball, return array with index 0
+if both balls have colliding directions then
+	if strengths are equal, both balls are destroyed, return no index
+	if one ball's strength is greater, than the other is destroyed, return the index of the first ball
+if both balls have the same direction return both indexes
+if the balls have oposite directions return both indexes */
+
 function findRemainingBalls(direction, strength) {
 	// Write your code here
 	if (direction.length !== strength.length) {
 		return -1;
 	}
+	if (direction.length === 1) {
+		return [0];
+	}
+	let result = [];
+	let i = 0;
+
+	if(direction[i] === direction[i+1] || direction[i] < direction[i+1]) {
+		result.push(i, i+1)
+	}
+
+	// let result = [...Array(direction.length).keys()];
+
+	// for (let i = 0; i < direction.length; i++) {
+	// 	result.push(i)
+	// }
+
 	for (let i = 0; i < direction.length; i++) {
-		if (direction[i] > strength[i + 1]) {
+
+		if (i === 0 && direction[i] === -1) {
+			result.push(i);
+			continue;
+		}
+
+		if (i === direction.length - 1){
+			break;
+		}
+
+		if (direction[i] > direction[i + 1]) {
 			if (strength[i] > strength[i + 1]) {
-				return i;
+				result.push(i);
+			} else if (strength[i] < strength[i + 1]) {
+				result.push(i + 1);
 			} else {
-				return i + 1;
 			}
+		} else if (direction[i] < direction[i + 1]) {
+		} else {
 		}
 	}
-	return 1;
+	return result;
 }
 
 function main() {
-	const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+	const ws = fs.createWriteStream('./output');
 
 	const directionCount = parseInt(readLine().trim(), 10);
 
 	let direction = [];
 
 	for (let i = 0; i < directionCount; i++) {
+		console.log('Direction Count: ' + directionCount);
 		const directionItem = parseInt(readLine().trim(), 10);
+		console.log('Direction Item: ' + directionItem);
 		direction.push(directionItem);
 	}
 
@@ -65,3 +111,11 @@ function main() {
 
 	ws.end();
 }
+
+/* Edge cases
+[1,-1] [1,1]
+ */
+// 
+// Edge cases
+// [1,-1,1,-1,-1,1,-1,-1,1,1]
+// 
