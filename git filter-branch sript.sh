0fa62c891f83550f8b693c9592521f47dddd22c4
andrei <git1@email.com>
2023-01-20 15:36:12 +0100

    Update git filter-branch sript.sh

--- git filter-branch sript.sh
+++ git filter-branch sript.sh
@@ -20,3 +20,9 @@ HEAD
 # git rebase -i aed76f1 master -x "GIT_COMMITTER_DATE='2020-09-22' git commit --amend --date='2020-09-22' --no-edit"
 # git rebase -i --root -x "GIT_COMMITTER_DATE='2021-02-09 12:00:00' git commit --amend --date='2021-02-09 12:00:00' --no-edit"
 # git rebase -i --root -x "GIT_COMMITTER_DATE='2021-03-17 10:09:41' git commit --amend --date='2021-03-17 10:09:41' --no-edit"
+
+# firebase deploy --only hosting
+# firebase deploy --only hosting:site-name (e.g. firebase deploy --only hosting:linkedin-clone9)
+
+# Undoing Your Last Commit (That Has Not Been Pushed)
+# git reset --soft HEAD~
